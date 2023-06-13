d752a00dcc0ab246900105dbfe6614a017ca6e3a
Daniel Müller <daniel.mueller@surnet.ch>
2023-06-13 16:33:19 +0200

    Update dist.js

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,16 +5,18 @@ import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
 test('throws file not found', async () => {
-  await expect(getMatrix('./not-found.json')).rejects.toThrow('ENOENT: no such file or directory, open \'./not-found.json\'')
+  await expect(getMatrix('./not-found.json')).rejects.toThrow(
+    "ENOENT: no such file or directory, open './not-found.json'"
+  )
 })
 
 test('returns expected result', async () => {
-  const matrix = await getMatrix('./__tests__/sample.json');
+  const matrix = await getMatrix('./__tests__/sample.json')
   await expect(matrix).toEqual({
     include: [
-      { key: 'key1', value: 'value1' },
-      { key: 'key2', value: 'value2' },
-      { key: 'key3', value: 'value3' }
+      {key: 'key1', value: 'value1'},
+      {key: 'key2', value: 'value2'},
+      {key: 'key3', value: 'value3'}
     ]
   })
 })
