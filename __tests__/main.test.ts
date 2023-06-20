47d19f58c3920ec4edb77d37b3d14f286870a0f2
Daniel Müller <daniel.mueller@surnet.ch>
2023-06-20 15:24:31 +0200

    Use Release Function on Releases only

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,4 @@
 import {getMatrix} from '../src/get-matrix'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
 test('throws file not found', async () => {
